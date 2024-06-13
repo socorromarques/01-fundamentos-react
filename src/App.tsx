4eeab59a0aae4595f0fcc3ccfa979039d42ec0c9
Socorro Marques <msocorrosmarques@gmail.com>
2024-06-13 06:20:49 -0300

    Criação do projeto Ignite Feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import { Header} from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+
+import styles from './App.module.css'
  
 import "./global.css"
+import { Sidebar } from './components/SideBar'
 
 export function App() {
   return(
     <div>
       <Header />
-      <Post 
-        author="Socorro Marques"
-        content="Olha eu aqui de novo criando um novo projeto react!"
-      />
-      <Post 
-        author="Felipe Simoes"
-        conent= "Que fishe!"
-      />  
+        <div className={styles.wrapper}>
+          <Sidebar /> 
+          <main>
+            <Post 
+            author="Socorro Marques"
+            content="Olha eu aqui de novo criando um novo projeto react!"
+            />
+            <Post 
+            author="Felipe Simoes"
+            content= "Que fishe!"
+            />  
+          </main>
+        </div>
     </div>
   )
 }
 
-export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import styles from './SideBar.module.css'
+import { PencilLine } from 'phosphor-react'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img src="https://media.istockphoto.com/id/1360521207/pt/foto/businessman-using-a-computer-analysis-for-process-and-workflow-automation-with-flowchart-a.jpg?s=1024x1024&w=is&k=20&c=SrI_0N2J48G-l-_BmiJ73dlnUzQejI__ff39qvDll-8="  />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src='https://github.com/socorromarques.png'/>
+                <strong>Socorro Marques</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">    
+                    <PencilLine size={20} />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
