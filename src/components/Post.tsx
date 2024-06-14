14b2a1b1dc52349ad54deb78688824b9c5cc2b18
Socorro Marques <msocorrosmarques@gmail.com>
2024-06-14 18:18:24 -0300

    Criação do arquivo componente Post e sua estilização

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,31 +1,44 @@
-import { Header } from "./components/Header";
-import { Post } from "./components/Post";
-import { Sidebar } from "./components/Sidebar";
 
-import styles from "./App.module.css";
-import "./global.css";
+import styles from "./Post.module.css";
 
 
 
-export function App() {
+export function Post() {
 
   return (
-    <div>
-      <Header />
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}> 
+          <img className={styles.avatar} src="https://github.com/socorromarques.png" /> 
+          <div className={styles.authorInfo}>
+            <strong>Socorro Marques</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
 
-      <div className={styles.wrapper}>
-        <Sidebar />
-        <main>
-          <Post
-            author="Socorro Marques"
-            content="Olha eu aqui de novo criando um novo projeto react!"
-          />
-          <Post
-            author="Felipe Simoes"
-            content="Que fishe!"
-          />
-        </main>
+        <time title="24 de junho às 08:13h" dateTime="2024-06-14 20:13:30">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+
+        </p>
+        
+        <p>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>{' '}
+          <a href="#">#nlw</a> {' '}
+          <a href="#">#rocketseat</a>/{' '}
+        </p>
+
       </div>
-    </div>
+
+
+      
+    </article>
   )
 }
